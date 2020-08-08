00d78bca11f03bb38c3411b7e3481610539c365f
parkwoorim <woorimprog@gmail.com>
2020-08-08 15:00:42 +0900

    [server/mobileRefectoring] "../vending/update" 리펙토링
    1. 설명 주석 추가
    2. 클라이언트 요청 데이터 검사 구문 추가
    3. 클라이언트 요청 데이터가 없거나 데이터 수정시 에러날 경우 "false"와 에러 메세지(msg) 응답
    
    Signed-off-by: parkwoorim <woorimprog@gmail.com>

--- server/routes/mobile.js
+++ server/routes/mobile.js
@@ -114,19 +114,30 @@ router.post('/vending/read', (req, res) => {
 
 // 자판기 수정 요청
 router.post('/vending/update', (req, res) => {
+    // 클라이언트가 요청한 데이터 저장
     const vending = req.body.vending;
-    
-    db.query(`UPDATE vendings SET vending_name=?, vending_description=?, vending_full_size=? WHERE serial_number=?;`, 
-        [vending.name, vending.description, vending.fullSize, vending.serialNumber], (err, result) => {
-            // 실패시 false 응답
-            if (err) {
-                console.log(err);
-                res.json({ success : false });
-            }
 
-            // 성공시 true 응답
-            res.json({ success : true });
-    });
+    // 클라이언트가 요청한 데이터가 있는지 검사
+    if (!String.isEmpty(userId)) {
+        // 클라이언트가 전송한 "vending" 이 있다면, 자판기 정보 수정
+        db.query(`UPDATE vendings SET vending_name=?, vending_description=?, vending_full_size=? WHERE serial_number=?;`, 
+            [vending.name, vending.description, vending.fullSize, vending.serialNumber], (err, result) => {
+                // 실패시 false 응답
+                if (err) {
+                    console.log(err);
+                    res.json({ success : false });
+                }
+
+                // 성공시 true 응답
+                res.json({ success : true });
+        });
+    } else {
+        // 클라이언트가 전송한 데이터가 없다면 false 반환
+        res.json({
+            success : false,
+            msg : "The vending of the server is empty."
+        });
+    }
 });
 
 // 자판기 삭제 요청
