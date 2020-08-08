b8e1c4b948dc4bb12b91766c48584657a689a4d8
parkwoorim <woorimprog@gmail.com>
2020-08-08 15:24:05 +0900

    [server/mobileRefectoring] "/mobile/login" 리펙토링
    1. 설명 주석 추가
    2. 클라이언트 요청 데이터 검사 구문 추가
    3. 클라이언트 요청 데이터가 비어있거나, 로그인 아이디가 DB에 없거나, 패스워드와 일치하지 않는 등의 에러시  "false"와 에러 메세지(msg) 응답
    
    Signed-off-by: parkwoorim <woorimprog@gmail.com>

--- server/routes/mobile.js
+++ server/routes/mobile.js
@@ -12,29 +12,53 @@ const String = require('../class/String.js');
 
 // 로그인 요청 및 응답
 router.post('/login', (req, res) => {
-    // 모바일 요청 데이터
+    // 클라이언트가 요청한 데이터 저장
     const user = {
         id : req.body.userId,
         passwd : req.body.userPasswd
     };
     
-    // DB 검사 => 요청 아이디와 패스워드가 일치하는 데이터를 찾아 검사한다.
-    db.query(`SELECT * FROM users WHERE user_id LIKE ?`, [user.id], (err, userDB) => {
-        // 에러시 실행
-        if (err) {
-            console.log(err);
-            res.json( { success : false } );
-        }
-        
-        // 아이디와 패스워드 비교
-        if (userDB != false && userDB[0].user_id == user.id && userDB[0].user_passwd == user.passwd)
-            // 아이디 패스워드 일치시 True 응답
-            res.json( { success : true } );
-        else 
-            // 일치하지 않으면 False 응답
-            res.json( { success : false } );
+    // 클라이언트가 요청한 데이터가 있는지 검사
+    if (!String.isEmpty(user.id)) {
+        // 클라이언트가 전송한 "userId" 가 있다면, DB 검사 => 요청 아이디와 패스워드가 일치하는 데이터를 찾아 검사한다.
+        db.query(`SELECT * FROM users WHERE user_id LIKE ?`, [user.id], (err, userDB) => {
+            // 실패시 "false" 응답
+            if (err) {
+                console.log(err);
+                res.json({ 
+                    success : false,
+                    msg : err
+                });
+            }
+            
+            // DB에 해당하는 아이디가 없을 경우 실행
+            if (String.isEmpty(userDB)) {
+                res.json({ 
+                    success : false,
+                    msg : "The ID does not exist."
+                });
+            }
 
-    });
+            // 아이디와 패스워드 비교
+            if (userDB != false && userDB[0].user_id == user.id && userDB[0].user_passwd == user.passwd)
+                // 아이디 패스워드 일치시 True 응답
+                res.json({ success : true });
+            else {
+                // 일치하지 않으면 False 응답
+                res.json({ 
+                    success : false,
+                    msg : "The ID and Password do not Match."
+                });
+            }
+
+        });
+    } else {
+        // 클라이언트가 전송한 데이터가 없다면 false 반환
+        res.json({
+            success : false,
+            msg : "The user datas of the server is empty."
+        });
+    }
 });
 
 // 회원가입 요청 및 응답
