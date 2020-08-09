9ec3af4d0e4e520f19af338c471c7491f271eea1
parkwoorim <woorimprog@gmail.com>
2020-08-09 21:21:52 +0900

    [server/updateDrinks] /mobile/drink/update 경로 구현 완료
    Signed-off-by: parkwoorim <woorimprog@gmail.com>

--- server/routes/mobile.js
+++ server/routes/mobile.js
@@ -215,7 +215,40 @@ router.post('/vending/delete', (req, res) => {
 
 // 음료 등록 및 수정
 router.post('/drink/update', (req, res) => {
+    // 클라이언트가 요청한 데이터 저장
+    const serialNumber = req.body.serialNumber,
+        drink = {
+            position : req.body.drink.position,
+            name : req.body.drink.name,
+            price : req.body.drink.price
+        };
 
+    // 클라이언트가 요청한 데이터가 있는지 검사
+    if (!String.isEmpty(serialNumber)) {
+        // 클라이언트가 전송한 "serialNumber" 가 있다면, 음료 정보 수정
+        db.query(`UPDATE drinks SET drink_name=?, drink_price=? WHERE serial_number=? AND drink_position=?;`, 
+            [drink.name, drink.price, serialNumber, drink.position], (err, result) => {
+                // 실패시 false 응답
+                if (err) {
+                    console.log(err);
+                    res.json({ 
+                        success : false,
+                        msg : err
+                    });
+                }
+
+                // 성공시 true 응답
+                res.json({ 
+                    success : true 
+                });
+        });
+    } else {
+        // 클라이언트가 전송한 데이터가 없다면 false 반환
+        res.json({
+            success : false,
+            msg : "The drink data of the server is empty."
+        });
+    }
 });
 
 // 모듈 내보내기
