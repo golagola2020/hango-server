ede55831658a7c5b41897e78d9ec02edd218cd0c
박우림 <woorimprog@gmail.com>
2020-09-28 23:57:02 +0900

    [admin/login] 파비콘 설정
    Signed-off-by: 박우림 <woorimprog@gmail.com>

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@
 // 기본 모듈 포함
 const express = require('express'), // 웹 프레임워크
   dotenv = require('dotenv').config(), // 환경변수 관리용 모듈
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  favicon = require('serve-favicon'); // 파비콘 모듈
 
 // 기본 인스턴스 생성
 const app = express(); // express를 사용하기 위한 인스턴스 생성
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 80;
 app.set('views', './client/views');                     // 뷰 엔진의 기본 경로 세팅
 app.set('view engine', 'ejs');                          // 뷰 엔진은 ejs 사용
 app.use(express.static(`${__dirname}/public`));
+app.use(favicon(`${__dirname}/public/images/favicon.ico`));
 app.use(bodyParser.json());                             // 클라이언트의 요청 데이터 중 json 객체를 파싱할 수 있게 하기 위함
 app.use(bodyParser.urlencoded({ extended: true }));     // body-parser의 기본 셋
 
