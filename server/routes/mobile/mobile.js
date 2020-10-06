617d1e415213a9524bc709df2da1b29ee0ab7270
박우림 <woorimprog@gmail.com>
2020-10-06 22:11:11 +0900

    [mobile/stats] 가공된 매출 데이터를 응답하도록 수정
    Signed-off-by: 박우림 <woorimprog@gmail.com>

--- server/routes/mobile/mobile.js
+++ server/routes/mobile/mobile.js
@@ -15,8 +15,8 @@ const String = require('./../../class/String.js'),
 router.post('/login', (req, res) => {
     // 클라이언트가 요청한 데이터 저장
     const user = {
-        id : req.body.userId,
-        passwd : req.body.userPasswd
+        id: req.body.userId,
+        passwd: req.body.userPasswd
     };
 
     // 클라이언트의 요청 데이터를 터미널에 출력
@@ -25,7 +25,7 @@ router.post('/login', (req, res) => {
 
     // 응답 객체 선언
     const response = {}
-    
+
     // 클라이언트가 요청한 데이터가 있는지 검사
     if (!String.isEmpty(user.id)) {
         // 클라이언트가 전송한 "userId" 가 있다면, DB 검사 => 요청 아이디와 패스워드가 일치하는 데이터를 찾아 검사한다.
@@ -66,12 +66,12 @@ router.post('/login', (req, res) => {
 router.post('/signup', (req, res) => {
     // 클라이언트가 요청한 데이터 저장
     const user = {
-            name : req.body.userName,
-            id : req.body.userId,
-            email : req.body.userEmail,
-            passwd : req.body.userPasswd
-        };
-    
+        name: req.body.userName,
+        id: req.body.userId,
+        email: req.body.userEmail,
+        passwd: req.body.userPasswd
+    };
+
     // 클라이언트의 요청 데이터를 터미널에 출력
     console.log('클라이언트 요청 경로 : /mobile/signup \n데이터 : ');
     console.log(req.body);
@@ -131,9 +131,9 @@ router.post('/user/read', (req, res) => {
                 // 응답 데이터 생성
                 response.success = true;
                 response.user = {
-                    id : result[0].user_id,
-                    name : result[0].user_name,
-                    email : result[0].user_email,
+                    id: result[0].user_id,
+                    name: result[0].user_name,
+                    email: result[0].user_email,
                 };
             }
 
@@ -167,7 +167,7 @@ router.post('/user/update', (req, res) => {
     // 클라이언트가 요청한 데이터가 있는지 검사
     if (!String.isEmpty(user)) {
         // 클라이언트가 전송한 "userId" 가 있다면 수정
-        db.query(`UPDATE users SET user_id=?, user_name=?, user_email=?, user_passwd=? WHERE user_id=?`, 
+        db.query(`UPDATE users SET user_id=?, user_name=?, user_email=?, user_passwd=? WHERE user_id=?`,
             [user.newId, user.name, user.email, user.newPasswd, user.id], (err, result) => {
                 if (err) {
                     // 실패시 "false" 응답
@@ -181,7 +181,7 @@ router.post('/user/update', (req, res) => {
                 // 데이터 응답
                 Http.printResponse(response);
                 res.json(response);
-        });
+            });
     } else {
         // 클라이언트가 전송한 데이터가 없다면 false 반환
         response.success = false;
@@ -236,10 +236,10 @@ router.post('/user/delete', (req, res) => {
 // 판매데이터 조회 요청 및 응답
 router.post('/stats/read', (req, res) => {
     // 클라이언트가 요청한 데이터 저장
-    const userId = req.body.userId;
+    const userId = 'rltn123';
 
     // 클라이언트의 요청 데이터를 터미널에 출력
-    console.log('클라이언트 요청 경로 : /mobile/stats/read \n데이터 : ');
+    console.log('클라이언트 조회 요청 경로 : /mobile/stats/read \n요청 데이터 : ');
     console.log(req.body);
 
     // 응답 객체 선언
@@ -248,7 +248,11 @@ router.post('/stats/read', (req, res) => {
     // 클라이언트가 요청한 데이터가 있는지 검사
     if (!String.isEmpty(userId)) {
         // 클라이언트가 전송한 "userId" 가 있다면 판매데이터 조회
-        db.query(`SELECT * FROM sales WHERE user_id=?`, [userId], (err, results) => {
+        db.query(`SELECT date_format(sale_date, '%y-%m') AS sale_date, SUM(drink_price) AS price
+      FROM sales 
+      WHERE user_id = ? AND sale_date > (NOW() - INTERVAL 1 YEAR)
+      GROUP BY month(sale_date)
+      ORDER BY sale_date;`, [userId], (err, results1) => {
             // 실패시 "false" 응답
             if (err) {
                 response.success = false;
@@ -256,27 +260,93 @@ router.post('/stats/read', (req, res) => {
             } else {
                 // 응답 데이터 생성
                 response.success = true;
-                response.sales = [];
+                response.users = {
+                    saleDate: [],
+                    price: []
+                };
 
-                // DB에서 받아온 데이터 전체 삽입
-                let sale = {};
-                for (let result of results) {
-                    // DB 데이터를 Object로 초기화
-                    sale = {
-                        serialNumber: result.serial_number,
-                        drinkName: result.drink_name,
-                        drinkPrice: result.drink_price,
-                        date: result.sale_date,
-                    }
-
-                    // 판매 데이터를 Array에 삽입
-                    response.sales.push(sale);
+                // 데이터 삽입
+                for (let result of results1) {
+                    response.users.saleDate.push(result.sale_date);
+                    response.users.price.push(result.price);
                 }
-            }
 
-            // 데이터 응답
-            Http.printResponse(response);
-            res.json(response);
+                db.query(`SELECT v.vending_name, date_format(s.sale_date, '%y-%m') AS sale_date, SUM(s.drink_price) AS price
+              FROM sales AS s
+              JOIN vendings AS v
+              ON s.serial_number = v.serial_number
+              WHERE s.user_id = ? AND s.sale_date > (NOW() - INTERVAL 1 YEAR)
+              GROUP BY month(s.sale_date), s.serial_number
+              ORDER BY s.serial_number, sale_date;`, [userId], (err, results2) => {
+                    // 실패시 "false" 응답
+                    if (err) {
+                        response.success = false;
+                        response.msg = err;
+                    } else {
+                        // 응답 데이터 생성
+                        response.vendings = {
+                            name: []
+                        };
+
+                        // 데이터 삽입
+                        for (let result of results2) {
+                            response.vendings.name.push(result.vending_name);
+                            response.vendings[result.vending_name] = {
+                                saleDate: [],
+                                price: []
+                            }
+                        }
+
+                        // 중복 제거
+                        response.vendings.name = Array.from(new Set(response.vendings.name));
+
+                        // 데이터 삽입
+                        for (let result of results2) {
+                            response.vendings[result.vending_name].saleDate.push(result.sale_date);
+                            response.vendings[result.vending_name].price.push(result.price);
+                        }
+
+                        db.query(`SELECT drink_name, date_format(sale_date, '%y-%m') AS sale_date, SUM(drink_price) AS price
+            FROM sales 
+            WHERE user_id = ? AND sale_date > (NOW() - INTERVAL 1 YEAR)
+            GROUP BY month(sale_date), drink_name
+            ORDER BY drink_name, sale_date;`, [userId], (err, results3) => {
+                            // 실패시 "false" 응답
+                            if (err) {
+                                response.success = false;
+                                response.msg = err;
+                            } else {
+                                // 응답 데이터 생성
+                                response.drinks = {
+                                    name: []
+                                };
+
+                                // 데이터 삽입
+                                for (let result of results3) {
+                                    response.drinks.name.push(result.drink_name);
+                                    response.drinks[result.drink_name] = {
+                                        saleDate: [],
+                                        price: []
+                                    }
+                                }
+
+                                // 중복 제거
+                                response.drinks.name = Array.from(new Set(response.drinks.name));
+
+                                // 데이터 삽입
+                                for (let result of results3) {
+                                    response.drinks[result.drink_name].saleDate.push(result.sale_date);
+                                    response.drinks[result.drink_name].price.push(result.price);
+                                }
+                            }
+
+                            // 데이터 응답
+                            Http.printResponse(response);
+                            res.json(response);
+                        });
+                    }
+                });
+            }
         });
     } else {
         // 클라이언트가 전송한 데이터가 없다면 false 반환
