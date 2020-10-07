926245bc96962007c9f3dae663171db0fe2f964b
parkwoorim <woorimprog@gmail.com>
2020-10-07 09:05:37 +0900

    [hash] 로그인 및 회원가입시 암호화 적용 완료
    Signed-off-by: parkwoorim <woorimprog@gmail.com>

--- server/routes/mobile/mobile.js
+++ server/routes/mobile/mobile.js
@@ -12,17 +12,17 @@ const express = require('express'),
 const String = require('./../../class/String.js'),
     Http = require('./../../class/Http.js');
 
-// 로그인 요청 및 응답
+// 로그인처리 요청 및 응답
 router.post('/login', (req, res) => {
     // 클라이언트가 요청한 데이터 저장
     const user = {
-        id: req.body.userId,
-        passwd: req.body.userPasswd
+        id: req.body.id,
+        passwd: req.body.passwd
     };
 
     // 클라이언트의 요청 데이터를 터미널에 출력
     console.log('클라이언트 요청 경로 : /mobile/login \n데이터 : ');
-    console.log(req.body);
+    console.log(user.id);
 
     // 응답 객체 선언
     const response = {}
@@ -38,29 +38,39 @@ router.post('/login', (req, res) => {
                 // 실패시 false 응답
                 response.success = false;
                 response.msg = err;
-            } else {
-                // 비밀번호 해쉬 값 찾기
-                crypto.pbkdf2(user.passwd, userDB[0].user_salt, 100000, 64, 'sha256', (err, key) => {
-                    passwd = key.toString('base64');
-                });
 
-                if (String.isEmpty(userDB)) {
-                    // DB에 해당하는 아이디가 없을 경우 실행
-                    response.success = false;
-                    response.msg = "The ID does not exist.";
-                } else if (userDB != false && userDB[0].user_id == user.id && userDB[0].user_passwd == passwd) {
-                    // 아이디 패스워드 일치시 True 응답
-                    response.success = true;
-                } else {
-                    // 일치하지 않으면 False 응답
-                    response.success = false;
-                    response.msg = "The ID and Password do not Match.";
-                }
+                // 데이터 응답
+                Http.printResponse(response);
+                res.json(response);
             }
 
-            // 데이터 응답
-            Http.printResponse(response);
-            res.json(response);
+            if (String.isEmpty(userDB)) {
+                // DB에 해당하는 아이디가 없을 경우 실행
+                response.success = false;
+                response.msg = "The ID does not exist.";
+
+                // 데이터 응답
+                Http.printResponse(response);
+                res.json(response);
+            } else {
+                // 비밀번호 해쉬 값 찾기
+                crypto.pbkdf2(user.passwd, userDB[0].user_salt, 100000, 64, 'sha512', (err, key) => {
+                    passwd = key.toString('hex');
+
+                    if (userDB != false && userDB[0].user_id == user.id && userDB[0].user_passwd == passwd) {
+                        // 아이디 패스워드 일치시 True 응답
+                        response.success = true;
+                    } else {
+                        // 일치하지 않으면 False 응답
+                        response.success = false;
+                        response.msg = "The ID and Password do not Match.";
+                    }
+
+                    // 데이터 응답
+                    Http.printResponse(response);
+                    res.json(response);
+                });
+            }
         });
     } else {
         // 클라이언트가 전송한 데이터가 없다면 false 반환
@@ -76,16 +86,11 @@ router.post('/login', (req, res) => {
 // 회원가입 요청 및 응답
 router.post('/signup', (req, res) => {
     // 클라이언트가 요청한 데이터 저장
-    const user = {
-        name: req.body.userName,
-        id: req.body.userId,
-        email: req.body.userEmail,
-        passwd: req.body.userPasswd
-    };
+    const user = req.body;
 
     // 클라이언트의 요청 데이터를 터미널에 출력
     console.log('클라이언트 요청 경로 : /mobile/signup \n데이터 : ');
-    console.log(req.body);
+    console.log(user.id);
 
     // 응답 객체 선언
     const response = {}
@@ -98,27 +103,28 @@ router.post('/signup', (req, res) => {
 
         // 비밀번호 암호화 => 해쉬 암호만들기
         crypto.randomBytes(64, (err, buf) => {
-            salt = buf.toString('base64');
+            salt = buf.toString('hex');
 
-            crypto.pbkdf2(user.passwd, salt, 100000, 64, 'sha256', (err, key) => {
-                passwd = key.toString('base64');
+            // 해쉬 만들기
+            crypto.pbkdf2(user.passwd, salt, 100000, 64, 'sha512', (err, key) => {
+                passwd = key.toString('hex');
+
+                // DB 등록
+                db.query(`INSERT INTO users(user_id, user_name, user_email, user_passwd, user_salt) VALUES(?, ?, ?, ?, ?)`, [user.id, user.name, user.email, passwd, salt], (err, result) => {
+                    if (err) {
+                        // 실패시 false 응답
+                        response.success = false;
+                        response.msg = err;
+                    } else {
+                        // 응답 데이터 생성
+                        response.success = true;
+                    }
+
+                    // 데이터 응답
+                    Http.printResponse(response);
+                    res.json(response);
+                });
             });
-        });
-
-        // DB 등록
-        db.query(`INSERT INTO users(user_id, user_name, user_email, user_passwd, user_salt) VALUES(?, ?, ?, ?, ?)`, [user.id, user.name, user.email, passwd, salt], (err, result) => {
-            if (err) {
-                // 실패시 false 응답
-                response.success = false;
-                response.msg = err;
-            } else {
-                // 응답 데이터 생성
-                response.success = true;
-            }
-
-            // 데이터 응답
-            Http.printResponse(response);
-            res.json(response);
         });
     } else {
         // 클라이언트가 전송한 데이터가 없다면 false 반환
